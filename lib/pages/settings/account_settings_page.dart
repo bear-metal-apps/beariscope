e4e45a53a6433d2c91da62cf4b8079e5f69ff4d8
Jack J <115509970+sailboat3@users.noreply.github.com>
2025-12-07 18:56:37 -0800

    Revert test ui in acct settings

--- lib/pages/settings/account_settings_page.dart
+++ lib/pages/settings/account_settings_page.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:libkoala/providers/api_key_provider.dart';
-import 'package:libkoala/providers/auth_provider.dart';
-import 'package:libkoala/providers/graphql_provider.dart';
 
 class AccountSettingsPage extends ConsumerStatefulWidget {
   const AccountSettingsPage({super.key});
@@ -18,31 +14,7 @@ class _AccountSettingsPageState extends ConsumerState<AccountSettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Account')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            try {
-              final result = await ref
-                  .read(graphqlProvider)
-                  .query(
-                    QueryOptions(
-                      document: gql("""
-                        query {
-                          team(teamKey: "frc2046") {
-                            rookieYear
-                          }
-                        }
-                   """),
-                    ),
-                  );
-              print(result.data?['team']['rookieYear']);
-            } catch (e) {
-              print('Unexpected error: $e');
-            }
-          },
-          child: const Text('Get Team Data'),
-        ),
-      ),
+      body: Center(child: const Text('Account')),
     );
   }
 }
