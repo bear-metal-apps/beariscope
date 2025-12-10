dd44d2f3c46b4d4131359a1ef2b9bed3a69d4d22
Ryan_Yeo_753 <stmallworldforever@gmail.com>
2025-12-09 16:32:38 -0800

    General framework complete
    Need TeamCard in order to finish
    Date:12-9-2025, Day 1

---
+++ lib/pages/team_lookup/team_card_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+class CardList extends Notifier<List<Card>> {
+
+  @override
+  List<Card> build() {
+    return [];
+  }
+}
--- lib/pages/team_lookup/team_lookup_page.dart
+++ lib/pages/team_lookup/team_lookup_page.dart
@@ -1,6 +1,7 @@
 import 'package:beariscope/pages/main_view.dart';
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class TeamLookupPage extends StatefulWidget {
   const TeamLookupPage({super.key});
@@ -10,27 +11,46 @@ class TeamLookupPage extends StatefulWidget {
 }
 
 class _TeamLookupPageState extends State<TeamLookupPage> {
+  TextEditingController _searchTermTEC = TextEditingController();
+  List<Widget> filteredTeamCards = [];
+
+  // final allTeamCards = ref.read(teamCardListNotifierProvider);
+  // final filteredTeamCards = ref.read(filteredListNotifierProvider);
+  //
+  // void filter() {
+  //   int _intSearchTerm = int.tryParse(_searchTermTEC.text) ?? -1;
+  //   ref.read(filteredListNotifierProvider.notifier).reset();
+  //   if (_intSearchTerm < 0) {
+  //     for (TeamCard checkedCard in allTeamCards) {
+  //       if (checkedCard.teamNumber.contains('$_searchTermTEC')) {
+  //         ref.read(filteredListNotifierProvider.notifier).addCard(searchedCard);
+  //   } else {
+  //     for (TeamCard checkedCard in allTeamCards) {
+  //       if (checkedCard.teamName.contains('$_searchTermTEC')) {
+  //         ref.read(filteredListNotifierProvider.notifier).addCard(searchedCard);
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
-    final controller = TextEditingController();
     final main = MainViewController.of(context);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         titleSpacing: 8.0,
         title: SearchBar(
-          controller: controller,
+          controller: _searchTermTEC,
           hintText: 'Team name or number',
           padding: const WidgetStatePropertyAll<EdgeInsets>(
             EdgeInsets.symmetric(horizontal: 16.0),
           ),
           leading: const Icon(Icons.search_rounded),
-
           trailing: [
             IconButton(
               icon: Icon(Icons.filter_list_rounded),
               tooltip: 'Filter & Sort',
               onPressed: () {},
+              // onPressed: () {filter();},
             ),
           ],
         ),
@@ -43,7 +63,12 @@ class _TeamLookupPageState extends State<TeamLookupPage> {
                 ),
         actions: [SizedBox(width: 48)],
       ),
-      body: const Center(child: Text('Team Lookup Page')),
+      body: const Center(
+        child: Column(
+          children: filteredTeamCards;
+        )
+      ),
     );
   }
 }
+
