51e63990ac1de3c9f5c804fa0cfbcf4108d9870f
Tiny <wisptheproto@gmail.com>
2025-12-16 15:44:07 -0800

    All of this is just the up next page

--- lib/pages/up_next/up_next_page.dart
+++ lib/pages/up_next/up_next_page.dart
@@ -1,4 +1,5 @@
 import 'package:beariscope/pages/main_view.dart';
+import 'package:beariscope/pages/up_next/up_next_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
@@ -9,17 +10,36 @@ class UpNextPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = MainViewController.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Up Next'),
-        leading:
-            controller.isDesktop
-                ? null
-                : IconButton(
-                  icon: const Icon(Symbols.menu_rounded),
-                  onPressed: controller.openDrawer,
-                ),
-      ),
-      body: const Center(child: Text('Up Next Page')),
+        appBar: AppBar(
+          title: Text('Up Next'),
+          leading:
+          controller.isDesktop
+              ? null
+              : IconButton(
+            icon: const Icon(Symbols.menu_rounded),
+            onPressed: controller.openDrawer,
+          ),
+        ),
+        body: Center(
+          child: Column(
+
+            children: [UpNextWidget(
+              match: 'Qualification 11',
+              time: 'Next match in 20 Minutes',
+              onPressed: () {
+                print('meow');
+              },
+            ),
+              UpNextWidget(
+                match: 'Qualification 12',
+                time: 'Next match in 30 Minutes',
+                onPressed: () {
+                  print('meow');
+                },
+              ),
+            ],
+          ),
+        )
     );
   }
 }
