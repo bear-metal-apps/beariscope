4c3bc683b9ca906a5f7497b382c39403d014a1a7
Jack J <115509970+sailboat3@users.noreply.github.com>
2025-12-16 16:11:33 -0800

    Change dropdown to use m3

--- lib/pages/pits_scouting/pits_scouting_widgets.dart
+++ lib/pages/pits_scouting/pits_scouting_widgets.dart
@@ -139,18 +139,19 @@ class _DropdownButtonState extends State<DropdownButtonOneChoice> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      onChanged: (String? value) {
+    return DropdownMenu<String>(
+      initialSelection: dropdownValue,
+      onSelected: (String? value) {
         setState(() {
           dropdownValue = value!;
         });
       },
-      items:
-          list.map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(value: value, child: Text(value));
-          }).toList(),
+      dropdownMenuEntries: list.map<DropdownMenuEntry<String>>((String value) {
+        return DropdownMenuEntry<String>(
+          value: value,
+          label: value,
+        );
+      }).toList(),
     );
   }
 }
