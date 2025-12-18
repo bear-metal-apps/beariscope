c0e1316932481b73664500ba28aee03aa1af9af1
Jack J <115509970+sailboat3@users.noreply.github.com>
2025-12-18 15:23:59 -0800

    Cosmetic changes

--- lib/pages/up_next/up_next_widget.dart
+++ lib/pages/up_next/up_next_widget.dart
@@ -6,72 +6,33 @@ class UpNextWidget extends StatelessWidget {
   final VoidCallback onPressed;
 
   const UpNextWidget({
-    Key? key,
+    super.key,
     required this.match,
     required this.time,
     required this.onPressed,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child:Container(
-        width: MediaQuery.of(context).size.width * 0.6,
-        height: 100,
-        padding: EdgeInsets.all(10.0),
-        margin: EdgeInsets.all(20.0),
-
+      child: Container(
+        constraints: BoxConstraints(maxWidth: 800),
+        padding: EdgeInsets.all(24.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
           children: [
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.2,
-                    child: FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Text(
-                        match,
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                  ),
+                Text(
+                  match,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
                 ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.2,
-                    child: FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Text(time, style: TextStyle(fontSize: 18)),
-                    ),
-                  ),
-                ),
+                Text(time),
               ],
             ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.2,
-
-                child: OutlinedButton(
-                  onPressed: onPressed,
-                  child: FittedBox(
-                    fit: BoxFit.scaleDown,
-                    child: Text('Next Event', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-              ),
-            ),
+            OutlinedButton(onPressed: onPressed, child: Text('Button')),
           ],
         ),
       ),
