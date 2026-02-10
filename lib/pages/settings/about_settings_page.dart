0a816de29c8ba00c20dab4675e6c04c1cb5315a8
Turquoisiops <stmallworldforever@gmail.com>
2026-02-09 16:42:53 -0800

    feat:add about page
    closes #19
    
    ---------
    
    Co-authored-by: Jack J <115509970+jack3j@users.noreply.github.com>
    Co-authored-by: bearitone[bot] <259946569+bearitone[bot]@users.noreply.github.com>

--- lib/pages/settings/about_settings_page.dart
+++ lib/pages/settings/about_settings_page.dart
@@ -1,5 +1,7 @@
+import 'package:beariscope/components/beariscope_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class AboutSettingsPage extends ConsumerWidget {
   const AboutSettingsPage({super.key});
@@ -8,7 +10,26 @@ class AboutSettingsPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: const Text('About')),
-      body: Center(child: const Text('About')),
+      body: BeariscopeCardList(
+        children: [
+          BeariscopeCard(
+            title: 'Version',
+            trailing: FutureBuilder<PackageInfo>(
+              future: PackageInfo.fromPlatform(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Text('Loading');
+                } else if (snapshot.hasError) {
+                  return const Text('Error');
+                } else {
+                  final version = snapshot.data?.version ?? 'Unknown';
+                  return Text(version);
+                }
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
