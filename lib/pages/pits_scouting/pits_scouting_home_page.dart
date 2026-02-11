6e4937b264d17d226e00385ec62538d990e77a23
Ryan_Yeo_753 <stmallworldforever@gmail.com>
2026-02-10 17:34:28 -0800

    SearchBar works
    yay
    Date: 2-10-2026, Coding Day 2
    Commit ID: 2

--- lib/pages/pits_scouting/pits_scouting_home_page.dart
+++ lib/pages/pits_scouting/pits_scouting_home_page.dart
@@ -5,6 +5,13 @@ import 'package:material_symbols_icons/symbols.dart';
 
 import 'package:beariscope/pages/pits_scouting/pits_scouting_assets.dart';
 
+class Team {
+  String teamName;
+  int teamNumber;
+
+  Team(this.teamName, this.teamNumber);
+}
+
 class PitsScoutingHomePage extends ConsumerStatefulWidget {
   const PitsScoutingHomePage({super.key});
 
@@ -14,153 +21,93 @@ class PitsScoutingHomePage extends ConsumerStatefulWidget {
 }
 
 class PitsScoutingHomePageState extends ConsumerState<PitsScoutingHomePage> {
+  final List<Team> teams = [
+    Team('Bear Metal', 2046),
+    Team('SPARX', 1126),
+    Team('KB Bot', 3311),
+    Team('ASSIT', 2214),
+    Team('iDeer', 9427),
+    Team('Team R.O.B.O.T.I.C.S.', 107),
+    Team('Scared Heart Outlanders', 10285),
+    Team('The Wolfbotz', 4634),
+    Team('The Ninjaneers', 1068),
+    Team('ASCTE', 8605),
+    Team('Mets Robotics', 4745),
+    Team('Tiger-OPS Robotics', 7214),
+    Team('ROBOLYNX', 10907),
+    Team('Clarksville Cyber Storm', 11037),
+    Team('Power Struck Girls', 5965),
+    Team('Team Terminator', 5576),
+    Team('Robot Dolphins From Outer Space', 5199),
+    Team('Cyber Courgars', 6089),
+    Team('Manic Mechanics', 1851),
+    Team('Wildcogs', 3123),
+    Team('Weber Fever', 1724),
+    Team('--', 1364),
+    Team('The Marist Manta Rays', 6772),
+    Team('SpicyBots', 8138),
+    Team('Panteras', 2283),
+    Team('The Ducksons', 10508),
+    Team('Rambots', 2204),
+    Team('Greased Lightning', 4379),
+  ];
+
+  List<Team> filteredTeams = [];
+  List<PitsScoutingTeamCard> teamCards = [];
+
+  bool isInt(String input) => int.tryParse(input) != null;
+
+  List<Team> searchTeams(List<Team> list) {
+    return list;
+  }
+
+  List<Team> byName(String name, List<Team> list) {
+    final modifiedName = name.toLowerCase();
+    return list.where((t) => t.teamName.toLowerCase().contains(modifiedName)).toList();
+  }
+
+  List<Team> byNumber(int number, List<Team> list) {
+    return list
+        .where((t) => t.teamNumber.toString().contains(number.toString()))
+        .toList();
+  }
+
+  void filter(String query, List<Team> fullList) {
+    if (query.isEmpty) {
+      filteredTeams = fullList;
+      return;
+    }
+
+    if (isInt(query)) {
+      filteredTeams = byNumber(int.parse(query), fullList);
+    } else {
+      filteredTeams = byName(query, fullList);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    for (int i = 0; i < teams.length; i++) {
+      teamCards.add(
+        PitsScoutingTeamCard(
+          teamName: teams[i].teamName,
+          teamNumber: teams[i].teamNumber,
+          cardID: i,
+        ),
+      );
+    }
+
+    filteredTeams = teams;
+  }
+
+  final TextEditingController searchTermTEC = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController searchTermTEC = TextEditingController();
     final main = MainViewController.of(context);
-    List<Widget> teams = [
-      PitsScoutingTeamCard(
-        teamName: 'Bear Metal',
-        teamNumber: 2046,
-        cardID: 1,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'SPARX',
-        teamNumber: 1126,
-        cardID: 2,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'KB Bot',
-        teamNumber: 3311,
-        cardID: 3,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'ASSIT',
-        teamNumber: 2214,
-        cardID: 4,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'iDeer',
-        teamNumber: 9427,
-        cardID: 5,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Team R.O.B.O.T.I.C.S.',
-        teamNumber: 107,
-        cardID: 6,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Sacred Heart Outlanders',
-        teamNumber: 10285,
-        cardID: 7,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'The Wolfbotz',
-        teamNumber: 4634,
-        cardID: 8,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'The Ninjaneers',
-        teamNumber: 1068,
-        cardID: 9,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'ASCTE',
-        teamNumber: 8605,
-        cardID: 10,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Mets Robotics',
-        teamNumber: 4745,
-        cardID: 11,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Tiger-OPS Robotics',
-        teamNumber: 7214,
-        cardID: 12,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'ROBOLYNX',
-        teamNumber: 10907,
-        cardID: 13,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Clarksville Cyber Storm',
-        teamNumber: 11037,
-        cardID: 14,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Power Struck Girls',
-        teamNumber: 5965,
-        cardID: 15,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Team Terminator',
-        teamNumber: 5576,
-        cardID: 16,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Robot Dolphins From Outer Space',
-        teamNumber: 5199,
-        cardID: 17,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Cyber Cougars',
-        teamNumber: 6089,
-        cardID: 18,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Manic Mechanics',
-        teamNumber: 1851,
-        cardID: 19,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Wildcogs',
-        teamNumber: 3123,
-        cardID: 20,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Weber Fever',
-        teamNumber: 1724,
-        cardID: 21,
-      ),
-      PitsScoutingTeamCard(
-        teamName: '--',
-        teamNumber: 1364,
-        cardID: 22,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'The Marist Manta Rays',
-        teamNumber: 6772,
-        cardID: 23,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'SpicyBots',
-        teamNumber: 8138,
-        cardID: 24,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Panteras',
-        teamNumber: 2283,
-        cardID: 25,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'The Ducksons',
-        teamNumber: 10508,
-        cardID: 26,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Rambots',
-        teamNumber: 2204,
-        cardID: 27,
-      ),
-      PitsScoutingTeamCard(
-        teamName: 'Greased Lighting',
-        teamNumber: 4379,
-        cardID: 28,
-      ),
-    ];
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -179,20 +126,28 @@ class PitsScoutingHomePageState extends ConsumerState<PitsScoutingHomePage> {
               onPressed: () {},
             ),
           ],
+          onChanged: (value) {
+            setState(() => filter(value, teams));
+          },
         ),
-        leading:
-            main.isDesktop
-                ? SizedBox(width: 40)
-                : IconButton(
-                  icon: const Icon(Symbols.menu_rounded),
-                  onPressed: main.openDrawer,
-                ),
+        leading: main.isDesktop
+            ? SizedBox(width: 40)
+            : IconButton(
+          icon: const Icon(Symbols.menu_rounded),
+          onPressed: main.openDrawer,
+        ),
       ),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: teams
+            children: filteredTeams.map((team) {
+              return PitsScoutingTeamCard(
+                teamName: team.teamName,
+                teamNumber: team.teamNumber,
+                cardID: teams.indexOf(team),
+              );
+            }).toList(),
           ),
         ),
       ),
