a7eeea532cb74c07b3ee2a71e032db7683dc9066
Ryan_Yeo_753 <stmallworldforever@gmail.com>
2026-02-16 11:34:57 -0800

    Saving bcz no errors #2
    Stuff is broken tho
    Date: 2-16-2026, Coding Day 4
    Commit ID: 7

--- lib/pages/pits_scouting/pits_scouting_home_page.dart
+++ lib/pages/pits_scouting/pits_scouting_home_page.dart
@@ -9,150 +9,97 @@ import 'package:beariscope/pages/pits_scouting/pits_scouting_assets.dart';
 import 'package:beariscope/pages/team_lookup/team_lookup_page.dart';
 
 class Team {
-  String teamName;
-  int teamNumber;
+  final String teamName;
+  final int teamNumber;
 
   Team(this.teamName, this.teamNumber);
 }
+
+final allTeamsProvider = getListDataProvider(endpoint: '/teams');
+
+final currentEventTeamsProvider = Provider((ref) {
+  return getListDataProvider(endpoint: '/teams?event=${ref.watch(currentEventProvider)}');
+});
 
 class PitsScoutingHomePage extends ConsumerStatefulWidget {
   const PitsScoutingHomePage({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
+  ConsumerState<PitsScoutingHomePage> createState() =>
       PitsScoutingHomePageState();
 }
 
 class PitsScoutingHomePageState extends ConsumerState<PitsScoutingHomePage> {
   List<Team> teams = [];
   List<Team> filteredTeams = [];
-  List<PitsScoutingTeamCard> teamCards = [];
 
   Filter pitsFilter = Filter.allEvents;
 
   bool isInt(String input) => int.tryParse(input) != null;
 
-  List<Team> searchTeams(List<Team> list) {
-    return list;
-  }
-
-  List<Team> byName(String name, List<Team> list) {
-    final modifiedName = name.toLowerCase();
-    return list
-        .where((t) => t.teamName.toLowerCase().contains(modifiedName))
-        .toList();
-  }
-
-  List<Team> byNumber(int number, List<Team> list) {
-    return list
-        .where((t) => t.teamNumber.toString().contains(number.toString()))
-        .toList();
-  }
-
-  void filter(String query, List<Team> fullList) {
+  void filter(String query) {
     if (query.isEmpty) {
-      filteredTeams = fullList;
+      filteredTeams = teams;
       return;
     }
 
     if (isInt(query)) {
-      filteredTeams = byNumber(int.parse(query), fullList);
+      final number = int.parse(query);
+      filteredTeams = teams
+          .where((t) => t.teamNumber.toString().contains(number.toString()))
+          .toList();
     } else {
-      filteredTeams = byName(query, fullList);
+      final q = query.toLowerCase();
+      filteredTeams = teams
+          .where((t) => t.teamName.toLowerCase().contains(q))
+          .toList();
     }
   }
 
-  List<Widget> constructList(List<Map<String, String>> teams) {
-    return teams.map((team) {
-      final name = team["nickname"];
-      final number = team["teamNumber"];
-
-      return PitsScoutingTeamCard(
-        teamName: name ?? 'None',
-        teamNumber: int.tryParse(number!) ?? 0,
-        cardID: int.tryParse(number) ?? 0,
+  // ---------------------------------------------
+  // FIX 2: Typed model → no more Map<String, String>
+  // ---------------------------------------------
+  List<Team> mapTeams(List<dynamic> data) {
+    return data.map((item) {
+      return Team(
+        item["nickname"] as String,
+        int.parse(item["teamNumber"] as String),
       );
-    }).toList();
+    }).toList()
+      ..sort((a, b) => a.teamNumber.compareTo(b.teamNumber));
   }
-
-  Widget filterList(
-    AsyncValue<List<dynamic>> allTeams,
-    AsyncValue<List<dynamic>> currentEventTeams,
-    GetListDataProvider allTeamsProvider,
-    GetListDataProvider currentEventTeamsProvider,
-  ) {
-    if (pitsFilter == Filter.currentEventsOnly) {
-      return currentEventTeams.when(
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error:
-            (err, stack) => Center(
-              child: FilledButton(
-                onPressed: () => ref.invalidate(currentEventTeamsProvider),
-                child: const Text('Retry'),
-              ),
-            ),
-        data: (data) {
-          final allTeamData =
-              data.map((item) {
-                  return {
-                    "teamNumber": item["teamNumber"] as String,
-                    "nickname": item["nickname"] as String,
-                  };
-                }).toList()
-                ..sort((a, b) => a["teamNumber"]!.compareTo(b["teamNumber"]!));
-          return Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: constructList(allTeamData),
-              ),
-            ),
-          );
-        },
-      );
-    } else {
-      return allTeams.when(
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error:
-            (err, stack) => Center(
-              child: FilledButton(
-                onPressed: () => ref.invalidate(allTeamsProvider),
-                child: const Text('Retry'),
-              ),
-            ),
-        data: (data) {
-          final currentEventTeamData =
-              data.map((item) {
-                  return {
-                    "teamNumber": item["teamNumber"] as String,
-                    "nickname": item["nickname"] as String,
-                  };
-                }).toList()
-                ..sort((a, b) => a["teamNumber"]!.compareTo(b["teamNumber"]!));
-          return Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: constructList(currentEventTeamData),
-              ),
-            ),
-          );
-        },
-      );
-    }
+  Widget buildTeamList() {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          children: filteredTeams
+              .map((team) => PitsScoutingTeamCard(
+            teamName: team.teamName,
+            teamNumber: team.teamNumber,
+            cardID: team.teamNumber,
+          ))
+              .toList(),
+        ),
+      ),
+    );
   }
 
   final TextEditingController searchTermTEC = TextEditingController();
 
   @override
+  void dispose() {
+    searchTermTEC.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final main = MainViewController.of(context);
-    final allTeamsProvider = getListDataProvider(endpoint: '/teams');
-    final currentEventTeamsProvider = getListDataProvider(
-      endpoint: '/teams?event=${ref.read(currentEventProvider)}',
-    );
+
+    final currentEventTeamsProviderGet = ref.watch(currentEventTeamsProvider);
+
     final allTeamsAsync = ref.watch(allTeamsProvider);
-    final currentEventTeamsAsync = ref.watch(currentEventTeamsProvider);
+    final currentEventTeamsAsync = ref.watch(currentEventTeamsProviderGet);
 
     return Scaffold(
       appBar: AppBar(
@@ -167,19 +114,18 @@ class PitsScoutingHomePageState extends ConsumerState<PitsScoutingHomePage> {
           leading: const Icon(Icons.search_rounded),
           trailing: [
             PopupMenuButton(
-              icon: Icon(Icons.filter_list_rounded),
+              icon: const Icon(Icons.filter_list_rounded),
               tooltip: 'Filter & Sort',
-              itemBuilder:
-                  (context) => [
-                    PopupMenuItem(
-                      value: Filter.allEvents,
-                      child: Text('All Events'),
-                    ),
-                    PopupMenuItem(
-                      value: Filter.currentEventsOnly,
-                      child: Text('Current Event Only'),
-                    ),
-                  ],
+              itemBuilder: (context) => [
+                const PopupMenuItem(
+                  value: Filter.allEvents,
+                  child: Text('All Events'),
+                ),
+                const PopupMenuItem(
+                  value: Filter.currentEventsOnly,
+                  child: Text('Current Event Only'),
+                ),
+              ],
               onSelected: (Filter newValue) {
                 setState(() {
                   pitsFilter = newValue;
@@ -188,22 +134,47 @@ class PitsScoutingHomePageState extends ConsumerState<PitsScoutingHomePage> {
             ),
           ],
           onChanged: (value) {
-            setState(() => filter(value, teams));
+            setState(() {
+              filter(value);
+            });
           },
         ),
-        leading:
-            main.isDesktop
-                ? SizedBox(width: 40)
-                : IconButton(
-                  icon: const Icon(Symbols.menu_rounded),
-                  onPressed: main.openDrawer,
-                ),
+        leading: main.isDesktop
+            ? const SizedBox(width: 40)
+            : IconButton(
+          icon: const Icon(Symbols.menu_rounded),
+          onPressed: main.openDrawer,
+        ),
       ),
-      body: filterList(
-        allTeamsAsync,
-        currentEventTeamsAsync,
-        allTeamsProvider,
-        currentEventTeamsProvider,
+      body: pitsFilter == Filter.currentEventsOnly
+          ? currentEventTeamsAsync.when(
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (err, stack) => Center(
+          child: FilledButton(
+            onPressed: () =>
+                ref.invalidate(currentEventTeamsProvider),
+            child: const Text('Retry'),
+          ),
+        ),
+        data: (data) {
+          teams = mapTeams(data);
+          filteredTeams = teams;
+          return buildTeamList();
+        },
+      )
+          : allTeamsAsync.when(
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (err, stack) => Center(
+          child: FilledButton(
+            onPressed: () => ref.invalidate(allTeamsProvider),
+            child: const Text('Retry'),
+          ),
+        ),
+        data: (data) {
+          teams = mapTeams(data);
+          filteredTeams = teams;
+          return buildTeamList();
+        },
       ),
     );
   }
